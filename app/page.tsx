9b993103cd0708b18733bb2c5d66b4f641cce819
Arta RM <77877876+arutaruumu@users.noreply.github.com>
2025-06-12 11:52:55 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Features from "@/components/features-1";
 import ContentSection from "@/components/content-1";
 import CallToAction from "@/components/call-to-action";
 import FooterSection from "@/components/footer";
+import Script from '@/next/script';
 
 export default function Home() {
   return (
