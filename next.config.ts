a1da4e95f61e0173d5d452e3a2159a0574b9f609
Arta RM <77877876+arutaruumu@users.noreply.github.com>
2025-06-12 11:33:46 +0700

    --

---
+++ next.config.ts
@@ -0,0 +1,31 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.namu.wiki",
+      },
+      {
+        protocol: "https",
+        hostname: "www.gimbot.com",
+      },
+      {
+        protocol: "https",
+        hostname: "image.ggwp.id",
+      },
+      {
+        protocol: "https",
+        hostname: "static.wikia.nocookie.net",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
